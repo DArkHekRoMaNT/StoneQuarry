3743f6b0b10fc015bd796b4cd2ee3de07e3fe244
DArkHekRoMaNT <DArkHekRoMaNT@yandex.ru>
2023-05-04 15:10:45 +0300

    Add more info to config

--- Config.cs
+++ Config.cs
@@ -5,10 +5,24 @@ namespace StoneQuarry
     [Config("stonequarry.json")]
     public class Config
     {
+        [Description("Max total amount of sand, gravel and stones that can be stored in one rubble storage")]
+        [Range(1, int.MaxValue)]
         public int RubbleStorageMaxSize { get; set; } = 512;
+
+        [Description("Time of interaction with slabs to obtain stone in seconds")]
+        [Range(0f, 10f)]
         public float SlabInteractionTime { get; set; } = 0.2f;
+
+        [Description("Modifier of the difficulty (hits number) of hammering plugs")]
+        [Range(0f, 100f)]
         public float PlugWorkModifier { get; set; } = 1;
+
+        [Description("Modifier of plugs size (range)")]
+        [Range(0f, 100f)]
         public float PlugSizeModifier { get; set; } = 1;
+
+        [Description("Chance for break plug after use")]
+        [Range(0f, 1f)]
         public float BreakPlugChance { get; set; } = 0;
     }
 }
